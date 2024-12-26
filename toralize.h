da020d1eb2a89d5e014353cb47ec9181a572ef01
Mosawer Mahboob <masomahboob@gmail.com>
2024-12-26 06:26:06 +0430

    Define structers for sending/receiving SOCKS4 CONNECT packets

--- toralize.h
+++ toralize.h
@@ -7,3 +7,46 @@
 
 #define PROXY     "127.0.0.1"
 #define PROXYPORT 9050
+
+
+// type aliases for the packets
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+
+// SOCKS4 CONNECT implementation
+
+/*
+                +----+----+----+----+----+----+----+----+----+----+....+----+
+                | VN | CD | DSTPORT |      DSTIP        | USERID       |NULL|
+                +----+----+----+----+----+----+----+----+----+----+....+----+
+    # of bytes:	   1    1      2              4           variable       1
+*/
+
+// CONNECT request packet
+struct proxy_request {
+    int8 vn;
+    int8 cd;
+    int16 dstport;
+    int32 dstip;
+    unsigned char userid[8];
+};
+
+typedef struct proxy_request Req;
+
+/*
+                +----+----+----+----+----+----+----+----+
+                | VN | CD | DSTPORT |      DSTIP        |
+                +----+----+----+----+----+----+----+----+
+    # of bytes:	   1    1      2              4
+*/
+
+// CONNECT response packet
+struct proxy_response {
+    int8 vn;
+    int8 cd;
+    int16 _;
+    int32 __;
+};
+
+typedef struct proxy_request Res;
